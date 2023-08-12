0587a8036e46c728b73f162cdafb90e770257a89
MayconDietrich <maycon.dietrich@gmail.com>
2023-08-12 10:48:56 -0300

    feat: página de perfil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import NavBar from '../components/NavBar'
 import PokemonCard from '../components/PokemonCard'
-import { Container, Grid } from '@mui/material'
+import { Box, Container, Grid } from '@mui/material'
 import axios from 'axios'
 import { Skeletons } from '../components/Skeletons'
+import { useNavigate } from 'react-router-dom'
 
-export const Home = () => {
+export const Home = ({ setPokemonData }) => {
   const [pokemon, setPokemon] = useState([]);
+  const navigate = useNavigate();
+
   useEffect(() => {
     getPokemon();
   }, []);
@@ -33,6 +36,11 @@ export const Home = () => {
     setPokemon(filteredPokemon);
   }
 
+  const pokemonPickHandler = (pkmData) => {
+    setPokemonData(pkmData);
+    navigate('profile');
+  }
+
   return (
     <div>
       <NavBar pokemonFilter={pokemonFilter} />
@@ -40,13 +48,15 @@ export const Home = () => {
         <Grid container spacing={3}>
           {pokemon.length === 0 ? <Skeletons /> :
             pokemon.map((pkm, key) =>
-              <Grid item xs={12} sm={6} md={3} lg={2} key={key}  sx={{
+              <Grid item xs={12} sm={6} md={3} lg={2} key={key} sx={{
                 width: '100vw',
                 display: 'flex',
                 alignItems: 'center',
                 justifyContent: 'center',
-          }}>
-                <PokemonCard name={pkm.data.name} image={pkm.data.sprites.front_default} types={pkm.data.types} />
+              }}>
+                <Box onClick={() => pokemonPickHandler(pkm.data)} sx={{ cursor: 'pointer' }}>
+                  <PokemonCard name={pkm.data.name} image={pkm.data.sprites.front_default} types={pkm.data.types} />
+                </Box>
               </Grid>
             )
           }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import NavBar from '../components/NavBar'
+import { Box, Chip, Container, Divider, Paper, Typography } from '@mui/material'
+import PokemonTable from '../components/PokemonTable'
+import { useNavigate } from 'react-router-dom'
 
-export const Profile = () => {
+export const Profile = ({ pokemonData }) => {
+  const { name, sprites, moves } = pokemonData || {};
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!pokemonData) navigate('/');
+  }, []);
+
+  if (!pokemonData) return null;
+
   return (
-    <div>Profile</div>
+    <>
+      <NavBar hideSearch />
+      <Container maxWidth="md">
+        <Paper elevation={3}>
+          <Box display="flex" flexDirection="column" p={3}>
+            <Typography variant="h4">{name}</Typography>
+            <Box display="flex" alignItems="center" width="100%" marginBottom="16px" sx={{
+              flexDirection: {
+                xs: 'column', md: 'row'
+              }
+            }}>
+              <Box component="img" src={sprites.front_default} width="50%" height="50%" />
+              <PokemonTable pokemonData={pokemonData} />
+            </Box>
+          </Box>
+          <Box width="100%">
+            <Divider>Variações</Divider>
+            <Box display="flex" justifyContent="space-around">
+              <Box component="img" src={sprites.front_female} width="25%" height="25%" />
+              <Box component="img" src={sprites.front_shiny} width="25%" height="25%" />
+              <Box component="img" src={sprites.front_shiny_female} width="25%" height="25%" />
+            </Box>
+            <Divider>Ataques</Divider>
+            <Box textAlign="center" marginTop="16px">
+              {
+                moves.map((moveData, key) => <Chip key={key} sx={{ m: '5px' }} label={moveData.move.name} />)
+              }
+            </Box>
+          </Box>
+        </Paper>
+      </Container>
+    </>
   )
 }
