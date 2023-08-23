f15819831912bb2148c26e11fa9a19f035d05cbc
MayconDietrich <maycon.dietrich@gmail.com>
2023-08-23 19:15:01 -0300

    style: update profile page

--- src/components/PokemonTable/index.jsx
+++ src/components/PokemonTable/index.jsx
@@ -11,12 +11,12 @@ import { typeHandler } from '../../utils';
 
 
 export default function PokemonTable({ pokemonData }) {
-  const { height, weight, types } = pokemonData;
+  const { height, weight, types, id } = pokemonData;
 
   return (
     <TableContainer component={Paper} sx={{
       height: 'fit-content',
-      maxWidth: '250px',
+      maxWidth: '300px',
       boxShadow: 'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset',
       borderRadius: '5px',
       padding: '4px',
@@ -37,6 +37,10 @@ export default function PokemonTable({ pokemonData }) {
           <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
             <TableCell sx={{ fontWeight: 'bold' }}>Tipo: </TableCell>
             <TableCell>{typeHandler(types)}</TableCell>
+          </TableRow>
+          <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+            <TableCell sx={{ fontWeight: 'bold' }}>Pokedéx: </TableCell>
+            <TableCell>{id}</TableCell>
           </TableRow>
         </TableBody>
       </Table>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react'
 import NavBar from '../components/NavBar'
-import { Box, Chip, Container, Divider, Paper, Typography } from '@mui/material'
+import { Box, Chip, Container, Divider, Paper, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material'
 import PokemonTable from '../components/PokemonTable'
 import { useNavigate } from 'react-router-dom'
 import { typeColor } from '../utils'
 
 export const Profile = ({ pokemonData }) => {
-  console.log("🚀 ~ file: Profile.jsx:8 ~ Profile ~ pokemonData:", pokemonData)
   const { name, sprites, moves } = pokemonData || {};
   const navigate = useNavigate();
 
@@ -22,7 +21,7 @@ export const Profile = ({ pokemonData }) => {
       <NavBar hideSearch />
       <Container maxWidth="md" sx={{ marginBottom: '2rem' }}>
         <Paper elevation={3} sx={{
-          background: `linear-gradient(180deg, ${mainTypeColor} 0%, rgba(255,255,255,1) 80%)`,
+          background: `linear-gradient(180deg, ${mainTypeColor} 0%, rgba(255,255,255,1) 80%)`, padding: '16px'
         }}>
           <Box display="flex" flexDirection="column" p={3}>
             <Box display="flex" alignItems="center">
@@ -39,11 +38,60 @@ export const Profile = ({ pokemonData }) => {
             </Box>
           </Box>
           <Box width="100%">
-            <Divider>Variações</Divider>
-            <Box display="flex" justifyContent="space-around">
-              <Box component="img" src={sprites.front_female} width="25%" height="25%" />
-              <Box component="img" src={sprites.front_shiny} width="25%" height="25%" />
-              <Box component="img" src={sprites.front_shiny_female} width="25%" height="25%" />
+            {(sprites.front_female || sprites.front_shiny || sprites.front_shiny_female) &&
+              <>
+                <Divider>Variações</Divider>
+                <Box display="flex" justifyContent="space-around">
+                  <Box component="img" src={sprites.front_female} width="25%" height="25%" />
+                  <Box component="img" src={sprites.front_shiny} width="25%" height="25%" />
+                  <Box component="img" src={sprites.front_shiny_female} width="25%" height="25%" />
+                </Box>
+              </>
+            }
+            <Divider>Status</Divider>
+            <TableContainer component={Paper} sx={{
+              height: 'fit-content',
+              maxWidth: '500px',
+              margin: '0 auto',
+              padding: '4px',
+              display: 'flex',
+              justifyContent: 'center',
+              backgroundColor: 'transparent',
+            }}>
+              <Table aria-label="pokemon table">
+                <TableBody>
+                  <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell sx={{ fontWeight: 'bold' }}>HP: </TableCell>
+                    <TableCell>{pokemonData.stats[0].base_stat}</TableCell>
+                  </TableRow>
+                  <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell sx={{ fontWeight: 'bold' }}>Attack: </TableCell>
+                    <TableCell>{pokemonData.stats[1].base_stat}</TableCell>
+                  </TableRow>
+                  <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell sx={{ fontWeight: 'bold' }}>Defense: </TableCell>
+                    <TableCell>{pokemonData.stats[2].base_stat}</TableCell>
+                  </TableRow>
+                  <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell sx={{ fontWeight: 'bold' }}>Special Attack: </TableCell>
+                    <TableCell>{pokemonData.stats[3].base_stat}</TableCell>
+                  </TableRow>
+                  <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell sx={{ fontWeight: 'bold' }}>Special Defense: </TableCell>
+                    <TableCell>{pokemonData.stats[4].base_stat}</TableCell>
+                  </TableRow>
+                  <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell sx={{ fontWeight: 'bold' }}>Speed: </TableCell>
+                    <TableCell>{pokemonData.stats[5].base_stat}</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <Divider sx={{ marginTop: '32px' }}>Ataques</Divider>
+            <Box textAlign="center" marginTop="16px">
+              {
+                moves.map((moveData, key) => <Chip key={key} sx={{ m: '5px' }} label={moveData.move.name} />)
+              }
             </Box>
           </Box>
         </Paper>
